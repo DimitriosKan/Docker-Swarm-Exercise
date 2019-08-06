5f03e993706e3d1674aaf9efd394188c6178f2ca
DimitriosKan <mrhydro7k@gmail.com>
2019-08-06 00:11:50 +0000

    Base setup for terraforming a GCP VM :: edits requiredm outlined in README

---
+++ but_linux/main.tf
@@ -0,0 +1,36 @@
+# - - - CREATE PRIMARY VM INSTANCE - - - 
+resource "google_compute_instance" "main" {
+	name = "${var.name}-main"
+	machine_type = "${var.machine_type}"
+	zone = "${var.zone}"
+	tags = ["${var.name}"]
+	boot_disk {
+		initialize_params {
+			image = "${var.image}"
+		}
+	}
+	network_interface {
+		network = "${var.network}"
+		access_config {
+		
+		}
+	}
+	metadata = {
+		sshKeys = "terraform:${file("${var.public_key}")}"
+	}
+	connection {
+		type = "ssh"
+		user = "mrhyd"
+		host = "${google_compute_instance.main.network_interface.0.access_config.0.nat_ip}"
+		private_key = "${file("${var.private_key}")}"
+	}
+	provisioner "remote-exec" {
+		inline = [
+			"${var.update_packages[var.package_manager]}",
+			"${var.install_packages[var.package_manager]} ${join(" ", var.packages)}"
+		]
+	}
+	provisioner "remote-exec" {
+		scripts = ["scripts/main_tf_script"]
+	}
+}
