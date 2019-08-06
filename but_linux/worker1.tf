543d9b19d5ebd4e768d942740b32f987ec36ff5f
DimitriosKan <mrhydro7k@gmail.com>
2019-08-06 16:59:02 +0000

    setup of VM structure: will be adding scripts for guidance throughout Docker Swarm setup

---
+++ but_linux/worker1.tf
@@ -0,0 +1,41 @@
+# - - - CREATE PRIMARY VM INSTANCE - - - 
+resource "google_compute_instance" "worker1" {
+	name = "${var.name}-worker-boi-1${formatdate("DDmmss", timestamp())}"
+	machine_type = "${var.machine_type}"
+	zone = "${var.zone}"
+	tags = ["${var.name}"]
+	boot_disk {
+		initialize_params {
+			image = "${var.image}"
+		}
+	}
+	network_interface {
+		network = "${var.network}"
+		access_config {
+		
+		}
+	}
+	metadata = {
+		sshKeys = "terraform:${file("${var.public_key}")}"
+	}
+	connection {
+		type = "ssh"
+		user = "terraform"
+		host = "${google_compute_instance.worker1.network_interface.0.access_config.0.nat_ip}"
+		private_key = "${file("${var.private_key}")}"
+	}
+	provisioner "remote-exec" {
+		inline = [
+			"${var.update_packages[var.package_manager]}",
+			"${var.install_packages[var.package_manager]} ${join(" ", var.packages)}"
+		]
+	}
+	provisioner "remote-exec" {
+		scripts = [
+                  "scripts/test_script",
+                  "scripts/docker_tf_script",
+                  "scripts/docker-compose_tf_script"
+                ]
+	}
+}
+
